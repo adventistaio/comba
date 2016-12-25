4a9400111af6b990fb4aca689f124a0033b0ee5f
Oscar Tellez <oscartzgz@gmail.com>
2016-12-24 22:24:03 -0600

    Book has_many chapters

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :section
   belongs_to :category
   has_one :book_commentary
+  has_many :chapters
 
   accepts_nested_attributes_for :book_commentary
 end
