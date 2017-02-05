4f293158596c790eda0642c7895b5261477e2cac
Oscar Tellez <oscartzgz@gmail.com>
2017-02-04 21:20:27 -0600

    change routes and now verse commentary edition works

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,11 @@ module Comba
     config.assets.paths << Rails.root.join("ficons")
 
     # For ckeditor works on heroku
+    #config.assets.precompile += Ckeditor.assets
+    #config.assets.precompile += %w( ckeditor/* )
+    #config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.enabled = true
     config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
